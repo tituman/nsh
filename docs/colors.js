7d64baf3ba29909fd6c5f503add7eccc9f7b653f
tituman <tituman>
2025-04-04 17:53:57 +0900

    mvp for multiples

--- docs/colors.js
+++ docs/colors.js
@@ -8,11 +8,18 @@ let revert = true;
 var inputTimer = null;
 
 function createColorPickers() {
-  const btnArray = new Array(fullList.length);
+  const btnArray = new Array(len);
   const container = document.getElementById('colorPickerRow');
-  for (let i = 0; i < btnArray.length; i++) {
-    const picker = document.createElement('input');
+  for (let i = 0; i < len; i++) {
+    const pickerDiv = document.createElement('div');
+    const arrowLabels = document.createElement('div');
+    const arrowLabelRight = document.createElement('span');
+    const arrowLabelLeft = document.createElement('span');
+    arrowLabelRight.innerHTML = '➡️';
+    arrowLabelLeft.innerHTML = '⬅️';
+    picker = document.createElement('input');
     picker.type = 'color';
+    pickerDiv.className = 'color-picker-div';
     picker.className = 'color-picker';
     picker.value = '#ffffff';
 
@@ -27,18 +34,63 @@ function createColorPickers() {
     picker.addEventListener("dblclick", (event) => {
       alert('dblclck');
     });
-
-    container.appendChild(picker);
+    arrowLabelRight.addEventListener('click', (event) => {
+      fillNextRow(event);
+    });
+    arrowLabelLeft.addEventListener('click', (event) => {
+      fillPrevRow(event);
+    });
+    //dont add first element's left arrow
+    if (i > 0) arrowLabels.appendChild(arrowLabelLeft);
+    //dont add last element's right arrow 
+    if (i < (len - 1)) arrowLabels.appendChild(arrowLabelRight);
+    pickerDiv.appendChild(arrowLabels);
+    pickerDiv.appendChild(picker);
+    container.appendChild(pickerDiv);
     btnArray[i] = picker;
   }
 
 
   //mark the middle one
+  btnArray[22].previousSibling.innerHTML = 'mid';
   btnArray[22].className = 'color-picker-mid';
 
   return btnArray;
 }
 
+// user clicked on the arrow, so we fill the next row with same color
+/*
+<div class="color-picker-div">                              <---- 0 level parent
+    <div>                                                     <---- 1 level arrowLabels
+        <span>⬅️</span>                                        <---- 2 level arrow --> click target
+        <span>➡️</span>
+    </div>
+    <input type="color" class="color-picker">                 <---- 1 level
+</div>
+
+document.querySelector("#colorPickerRow > div:nth-child(21)")
+
+*/
+function fillNextRow(e) {
+  const clickTarget = e.target;   //2 level
+console.log('target: ', clickTarget);
+  const color = clickTarget.parentElement.nextSibling.value;  //1 level next sibling
+console.log('color : ', clickTarget.parentElement.nextSibling.value);
+  const nextRow = clickTarget.parentElement.parentElement.nextSibling.firstChild.nextSibling; // 1 level, 0 level, next div, 1 level, next sibling
+console.log('next row: ', nextRow);
+  nextRow.value = (nextRow)? color : '#001133';
+  fillColors();
+}
+function fillPrevRow(e) {
+  const clickTarget = e.target;   //2 level
+console.log('target: ', clickTarget);
+  const color = clickTarget.parentElement.nextSibling.value;  //1 level next sibling
+console.log('color : ', clickTarget.parentElement.nextSibling.value);
+  const prevRow = clickTarget.parentElement.parentElement.previousSibling.firstChild.nextSibling; // 1 level, 0 level, prev div, 1 level, next sibling
+console.log('next row: ', prevRow);
+prevRow.value = (prevRow)? color : '#001133';
+  fillColors();
+}
 
 function executeAfterTimer(redraw) {
   //timer function to delay triggering events too quickly
